81e4c636b420eb55beceba426c295f398d7f19da
sqh17 <852104785@qq.com>
2020-04-15 23:58:46 +0800

    promise1.js

--- ways/promise的实现/promise.md
+++ ways/promise的实现/promise.md
@@ -1,3 +1,14 @@
-------
 
-为了一步步实现promise的实现，分步进行，一个js文件代表一个过程
+promise的学习
+------
+为了研究es6的promise，手写promise。
+
+
+
+
+
+
+
+
+##### 参考博文：
+  [写代码像蔡徐抻 --- 异步编程二三事 | Promise/async/Generator实现原理解析](https://juejin.im/post/5e3b9ae26fb9a07ca714a5cc)
---
+++ ways/promise的实现/promise1.js
@@ -0,0 +1,44 @@
+
+class myPromise {
+  // 构造方法接收一个回调
+  constructor(executor){
+    this._resolveQueue = []    // then收集的执行成功的回调队列
+    this._rejectQueue = [] // then收集的执行失败的回调队列
+
+    let _resolve = (val)=>{
+      // 从成功队列里取出回调依次执行
+      while(this._resolveQueue.length){
+        let callback = this._resolveQueue.shift();
+        callback(val)
+      }
+    }
+
+    let _reject = (val)=>{
+      // 从失败队列里取出回调依次执行
+      while(this._rejectQueue.length){
+        let callback = this._rejectQueue.shift();
+        callback(val)
+      }
+    }
+
+    // new Promise()时立即执行executor,并传入resolve和reject
+    executor(_resolve,_reject)
+  }
+
+  
+  then(resolve,reject){
+    this._resolveQueue.push(resolve); // then之后直接push到各个队列中
+    this._rejectQueue.push(reject)
+  }
+}
+
+
+
+// 测试
+const p1 = new myPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('啦啦啦啦')
+  }, 1000);
+})
+p1.then(res => console.log(res))
+//一秒后输出啦啦啦啦
